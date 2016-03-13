362e5559fd22a9ba80f0317b487fe95f5d6d8051
duyutaochn <du_y_t@163.com>
2016-03-13 23:29:57 +0800

    提交初始代码

---
+++ hello.py
@@ -0,0 +1,24 @@
+import textwrap
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define("port", default=9000, help="run on the given port", type=int)
+
+class WrapHandler(tornado.web.RequestHandler):
+    
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', friendly user!')
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
